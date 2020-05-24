b9f3efbe7eca5d31f92b19bc2f5c72fdfa58d638
Wilfried Woivre <wilfried.woivre@gmail.com>
2020-05-24 17:43:27 +0200

    remove github token

--- generator.py
+++ generator.py
@@ -1,7 +1,19 @@
+import os
 from github import Github
 
 def run():
-  print('some works to do')
+  markdownPath = os.getenv("markdownPath")
+  templatePath = os.getenv("templatePath")
+
+  if (os.path.exists(markdownPath)): 
+    print(f"Markdown Path {markdownPath} exists)")
+  else:
+    raise Exception(f"Markdown Path {markdownPath} not found)")
+  
+  if (os.path.exists(templatePath)): 
+    print(f"Markdown Path {templatePath} exists)")
+  else:
+    raise Exception(f"Markdown Path {templatePath} not found)")
 
 if __name__ == "__main__":
   run()
